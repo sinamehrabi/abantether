8a59dd15f15dd67d4086c7035ecc60a47ef5fa33
Cna <cna@Cnas-MacBook-Pro.local>
2024-05-30 23:19:20 +0330

    Add balance to user model and Order model

--- order/models.py
+++ order/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    currency = models.CharField(max_length=20)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_settled = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['currency', 'is_settled']),
+        ]
