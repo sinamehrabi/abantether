e37e25f6b26cfa622444ccb8fc200816fd7b4f45
Cna <cna@Cnas-MacBook-Pro.local>
2024-05-31 16:35:02 +0330

    Add celery and define broker url in settings.py

--- abantether/settings.py
+++ abantether/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -123,3 +123,6 @@ REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
 
 }
+
+STATIC_ROOT = "/static/"
+CELERY_BROKER_URL = "redis://redis:6379/0"
