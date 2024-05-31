91f61bab36ca570fe3109a48d531b12f1b934ec6
Cna <cna@Cnas-MacBook-Pro.local>
2024-05-31 11:25:18 +0330

    Add service module

---
+++ order/service.py
@@ -0,0 +1,30 @@
+from decimal import Decimal
+
+from django.db import transaction
+from django.db.models import F
+from rest_framework.exceptions import NotAcceptable
+from rest_framework.response import Response
+
+from .models import User, Order
+
+
+class OrderService:
+    def __init__(self, user):
+        self.user = user
+
+    def create_order(self, currency: str, amount: Decimal, base_price: Decimal):
+        total_cost = amount * base_price
+        with transaction.atomic():
+            user_balance = User.objects.filter(id=self.user.id).select_for_update().values('balance').first()['balance']
+
+            if user_balance < total_cost:
+                raise NotAcceptable({'error': 'Insufficient balance'})
+
+            User.objects.filter(id=self.user.id).update(balance=F('balance') - total_cost)
+
+            Order.objects.create(
+                user=self.user,
+                currency=currency,
+                amount=amount,
+                price=total_cost
+            )
