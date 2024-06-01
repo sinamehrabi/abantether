d39b28d7294d788ece597008397f7213e9cf3209
Sina <sina@Admins-MacBook-Pro.local>
2024-06-01 11:30:28 +0330

    update docker compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,14 @@
 if [ "$MODE" = "web" ]; then
     echo "Waiting for mysql..."
     while ! nc -z mydb 3306; do
-      sleep 0.1
+      sleep 1
     done
+    echo "MySQL started"
 
     python manage.py migrate
     python manage.py collectstatic --no-input
-    python manage.py test
     python manage.py createadminuser --username admin --password admin --noinput --email admin@admin.com
 
-    echo "MySQL started"
     gunicorn --workers 3 --max-requests 250 --max-requests-jitter 20 abantether.wsgi:application --bind 0.0.0.0:8000
 
 
